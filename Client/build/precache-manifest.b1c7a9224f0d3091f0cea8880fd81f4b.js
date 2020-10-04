e778d0140c515fcf6581b62afb37bb20143dc2b9
Samer Alkhatib <conan.500@hotmail.com>
2020-10-04 18:42:36 +0300

    Build react app

---
+++ Client/build/precache-manifest.b1c7a9224f0d3091f0cea8880fd81f4b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6390591627c05ef37f0d52f8f6a2c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d123f50084fe36f6fbf",
+    "url": "/static/js/2.98af0d67.chunk.js"
+  },
+  {
+    "revision": "f1e34473a62e7790a39f",
+    "url": "/static/js/main.98fa85d7.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
